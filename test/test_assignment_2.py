224d0a3798a1e75bb930c4f6b377d175038e7f67
Jitrayu Punrattanapongs <jitrayu.p@gmail.com>
2021-10-25 11:28:05 +0200

    feat: add skeleton unit test for assignment_2.py

---
+++ test/test_assignment_2.py
@@ -0,0 +1,99 @@
+import unittest
+
+class TestAssignment2(unittest.TestCase):
+    
+    def test_exercises_00(self):
+        pass
+    
+    def test_exercises_01(self):
+        pass
+    
+    def test_exercises_02(self):
+        pass
+    
+    def test_exercises_03(self):
+        pass
+    
+    def test_exercises_04(self):
+        pass
+    
+    def test_exercises_05(self):
+        pass
+    
+    def test_exercises_06(self):
+        pass
+    
+    def test_exercises_07(self):
+        pass
+    
+    def test_exercises_08(self):
+        pass
+    
+    def test_exercises_09(self):
+        pass
+    
+    def test_exercises_10(self):
+        pass
+    
+    def test_exercises_11(self):
+        pass
+    
+    def test_exercises_12(self):
+        pass
+    
+    def test_exercises_13(self):
+        pass
+    
+    def test_exercises_14(self):
+        pass
+    
+    def test_exercises_15(self):
+        pass
+    
+    def test_exercises_16(self):
+        pass
+    
+    def test_exercises_17(self):
+        pass
+    
+    def test_exercises_18(self):
+        pass
+    
+    def test_exercises_19(self):
+        pass
+    
+    def test_exercises_20(self):
+        pass
+    
+    def test_exercises_21(self):
+        pass
+    
+    def test_exercises_22(self):
+        pass
+    
+    def test_exercises_23(self):
+        pass
+    
+    def test_exercises_24(self):
+        pass
+    
+    def test_exercises_25(self):
+        pass
+    
+    def test_exercises_26(self):
+        pass
+    
+    def test_exercises_27(self):
+        pass
+    
+    def test_exercises_28(self):
+        pass
+    
+    def test_exercises_29(self):
+        pass
+    
+    def test_exercises_30(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
