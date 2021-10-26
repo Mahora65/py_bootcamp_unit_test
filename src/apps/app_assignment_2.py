74687af5e955a0a726471e83d6d27a0d58264e0d
Terje Espedal <terje.m.espedal@gmail.com>
2021-10-26 20:59:39 +0200

    Add file uploader.

--- src/apps/app_assignment_2.py
+++ src/apps/app_assignment_2.py
@@ -1,6 +1,13 @@
 import streamlit as st
+import os
+
 
 def app():
     st.title("Assignment 2")
-    
+
     st.write("Assignment 2 result checker")
+
+    uploaded_file = st.file_uploader("Upload your assignment.py file here", type=['py'])
+    if uploaded_file is not None:
+        file_details = {"FileName": uploaded_file.name, "FileType": uploaded_file.type, "FileSize": uploaded_file.size}
+        st.write(file_details)
